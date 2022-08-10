ce49adcf28893ef5a7e7e0677b1f7c97fc69e1e7
James Bridge <james6547@towerswatson.com>
2022-08-10 17:15:03 +0100

    add save and load to loacal storage

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -6,9 +6,9 @@ import documentIcon from "../media/icon-document.svg";
 import deleteIcon from "../media/icon-delete.svg";
 import saveIcon from "../media/icon-save.svg";
 
-const TopNav = ({ docTitle, menuFunction, deleteFunction }) => {
+const TopNav = ({ docTitle, menuFunction, deleteFunction, saveFunction }) => {
   return (
-    <nav className="flex flex-row justify-between items-center">
+    <nav className="flex h-14 flex-row justify-between items-center">
       <button
         id="navSideBarBtn"
         className="flex justify-center items-center h-14 w-14 bg-slate-700 hover:bg-slate-800 active:bg-slate-900"
@@ -20,15 +20,22 @@ const TopNav = ({ docTitle, menuFunction, deleteFunction }) => {
         <img src={documentIcon} alt="document icon" className="p-4" />
         <p className="text-white">{docTitle}.md</p>
       </div>
-      <button className="mr-2 ml-2 p-2 rounded bg-slate-600 hover:bg-slate-700 active:bg-slate-900">
+      <button
+        onClick={deleteFunction}
+        className="mr-4 ml-4 rounded bg-slate-600 hover:bg-slate-700 active:bg-slate-900"
+      >
         <img
+          id="deleteDocBtn"
           src={deleteIcon}
           alt="delete icon"
           className="h-full"
-          onClick={deleteFunction}
         />
       </button>
-      <button className="mr-2 ml-2 p-2 rounded bg-red-400 hover:bg-red-500 active:bg-red-900">
+      <button
+        className="mr-2 ml-2 p-2 rounded bg-red-400 hover:bg-red-500 active:bg-red-900"
+        id="saveDocBtn"
+        onClick={saveFunction}
+      >
         <img src={saveIcon} alt="save icon" className="h-full" />
       </button>
     </nav>
