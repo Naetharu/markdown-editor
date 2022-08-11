1ef36e8e3ff8d79559048c10f0e679392a434811
James Bridge <james6547@towerswatson.com>
2022-08-11 18:08:38 +0100

    lots of changes i was lax on comitting

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -6,7 +6,13 @@ import documentIcon from "../media/icon-document.svg";
 import deleteIcon from "../media/icon-delete.svg";
 import saveIcon from "../media/icon-save.svg";
 
-const TopNav = ({ docTitle, menuFunction, deleteFunction, saveFunction }) => {
+const TopNav = ({
+  docTitle,
+  menuFunction,
+  deleteFunction,
+  saveFunction,
+  newDocumentFunction,
+}) => {
   return (
     <nav className="flex h-14 flex-row justify-between items-center">
       <button
@@ -17,26 +23,28 @@ const TopNav = ({ docTitle, menuFunction, deleteFunction, saveFunction }) => {
         <img src={menuIcon} alt="menu icon" />
       </button>
       <div className="flex justify-start grow items-center">
-        <img src={documentIcon} alt="document icon" className="p-4" />
-        <p className="text-white">{docTitle}.md</p>
+        <button onClick={newDocumentFunction}>
+          <img src={documentIcon} alt="document icon" className="p-4" />
+        </button>
+        <p className="text-white">{docTitle ? docTitle + ".md" : ""}</p>
       </div>
       <button
         onClick={deleteFunction}
-        className="mr-4 ml-4 rounded bg-slate-600 hover:bg-slate-700 active:bg-slate-900"
+        className="mr-2 ml-2 p-2 rounded bg-red-400 hover:bg-red-500 active:bg-red-900"
       >
         <img
           id="deleteDocBtn"
           src={deleteIcon}
           alt="delete icon"
-          className="h-full"
+          className="h-6"
         />
       </button>
       <button
-        className="mr-2 ml-2 p-2 rounded bg-red-400 hover:bg-red-500 active:bg-red-900"
+        className="mr-2 ml-2 p-2 rounded bg-blue-400 hover:bg-blue-500 active:bg-blue-900"
         id="saveDocBtn"
         onClick={saveFunction}
       >
-        <img src={saveIcon} alt="save icon" className="h-full" />
+        <img src={saveIcon} alt="save icon" className="h-6" />
       </button>
     </nav>
   );
