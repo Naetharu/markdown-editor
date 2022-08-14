41d24a78e110446ed01faf90caf48538ae8da021
James Bridge <james6547@towerswatson.com>
2022-08-14 21:11:16 +0100

    small CSS updates

--- src/components/SaveModal.jsx
+++ src/components/SaveModal.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 
-const SaveModal = ({ saveFunction }) => {
+const SaveModal = ({ saveFunction, closeFunction }) => {
   return (
-    <div className="h-[400px] w-[600px] bg-slate-600 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-center border-8 border-slate-700 rounded-xl">
+    <div className="h-[300px] w-[460px] bg-slate-600 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-center items-between border-4 border-slate-700 rounded">
+      <div className="w-full flex justify-end">
+        <button
+          className="p-1  text-slate-200 rounded font-semibold bg-slate-400 hover:bg-slate-500 active:bg-slate-600"
+          onClick={closeFunction}
+        >
+          Close
+        </button>
+      </div>
+
       <form
-        className="m-14 w-full grow flex flex-col justify-between items-center"
+        className="m-2 w-full grow flex flex-col justify-between items-center"
         onSubmit={saveFunction}
       >
-        <label htmlFor="docNameInput" className="text-3xl text-slate-300">
-          New Document
+        <label htmlFor="docNameInput" className="text-xl text-slate-300">
+          New Document Name:
         </label>
         <input
           id="docNameInput"
           type="text"
-          className="w-3/4 text-3xl p-4 bg-slate-100 rounded-xl"
+          className="w-3/4 text-xl p-2 bg-slate-100 rounded"
         />
         <button
           type="submit"
-          className="bg-slate-200 w-1/2 pt-4 pb-4 text-xl border-slate-400 border-8 rounded-xl hover:bg-slate-500 active:bg-slate-600"
+          className="bg-slate-200 w-1/2 pt-4 pb-4 text-xl border-slate-400 border-2 rounded hover:bg-slate-500 active:bg-slate-600"
         >
           SUBMIT
         </button>
